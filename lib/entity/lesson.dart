070c1fea039107017cd65440ece2448d89cd62aa
sshakndr <whenbored777@gmail.com>
2021-03-11 15:38:43 +0700

    implemented class Lesson

--- lib/entity/lesson.dart
+++ lib/entity/lesson.dart
@@ -1,3 +1,21 @@
 class Lesson {
-  //TODO
+  Lesson(this.name, this.group, this.type, this.start, this.finish, this.place);
+  final String name;
+  final String group;
+  final LessonType type;
+  final DateTime start;
+  final DateTime finish;
+  final String place;
+}
+enum LessonType{
+  PRACTICE,
+  LABORATORY,
+  LECTURE,
+  INDEPENDENT_WORK,
+  COURSEWORK,
+  COURSE_PROJECT,
+  EXAM,
+  WORK_PRACTICE,
+  OFFSET,
+  RATE_OFFSET,
 }
